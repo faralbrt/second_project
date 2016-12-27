034ca581ca54ebfd1442fede5f93069811c81186
faralbrt <albert.farhi5@gmail.com>
2016-12-27 18:57:11 -0500

    add excel functionality

--- model/controller.rb
+++ model/controller.rb
@@ -1,9 +1,14 @@
 require_relative 'scraper_module'
 require_relative 'csv_module'
+require_relative 'excel_module'
 require_relative 'watch'
 require_relative 'watch_list'
 
 # browser is set to start up in firefox using the compatible geckodriver.exe which is placed in the current path
+scrape_file = '../files/items_to_scrape.csv'
+url_file = '../files/all_urls.csv'
+scraped_data = []
+
 
 if ARGV.any?
 
@@ -17,12 +22,11 @@ if ARGV.any?
   when "update urls"
     puts "Estimated Run Time: 5 minutes"
     url_list = Scraper.scrape_all_urls(browser)
-    FileAccessor.overwrite_all('all_urls.csv', url_list)
+    FileAccessor.overwrite_all(url_file, url_list)
     browser.close
   when "scrape"
     count = 1
-    scrape_file = options.first
-    url_list = FileAccessor.parse_to_a('all_urls.csv')
+    url_list = FileAccessor.parse_to_a(url_file)
     watch_list = WatchList.new
     watches_to_scrape = watch_list.load_watches(scrape_file)
     watches_to_scrape.each do |watch|
@@ -34,10 +38,12 @@ if ARGV.any?
         scrape_info["model"] = watch.model
         p scrape_info
         FileAccessor.push_to_file('jomashop_other_results.csv', scrape_info.values)
+        scraped_data << scrape_info.values
         count += 1
         puts "Count - #{count}"
       end
     end
+    Excel.new_file(scraped_data)
   end
 
 end
---
+++ model/excel_module.rb
@@ -0,0 +1,26 @@
+require 'spreadsheet'
+
+module Excel
+  @file = '../files/results.xls'
+  @headers = %w{brand joma_model final_price availability shipping type_of_sale model}
+  @seed_data = [["hello"], ["the name is"], ["Albert"]]
+
+  def self.new_file(data = @seed_data)
+    File.delete(@file)
+    book = Spreadsheet::Workbook.new
+    book.create_worksheet :name => 'results'
+    sheet = book.worksheet 0
+    sheet.row(0).concat @headers
+    self.load_data(sheet, data)
+    book.write(@file)
+  end
+
+  def self.load_data(sheet, data)
+    row_count = 1
+    data.each do |seed_row|
+      sheet.row(row_count).concat seed_row
+      row_count += 1
+    end
+  end
+
+end
